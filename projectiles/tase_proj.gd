fbb20cec62800dee7bfc1cb5462064924f9ba9c8
albert@ankerdata.com <albert@ankerdata.com>
2025-12-23 18:42:29 +0200

    Got tase_proj animation working : D

--- projectiles/tase_proj.gd
+++ projectiles/tase_proj.gd
@@ -3,11 +3,11 @@ extends ProjectileBase
 @onready var anim_sprite: AnimatedSprite2D = $AnimSprite
 
 func _ready() -> void:
-	print("proj ready has run")
 	play_attack_animation()
 
 
 func play_attack_animation() -> void:
-	print("playing proj attack animation")
-	if anim_sprite.animation != "attack_animation":
-		anim_sprite.play("attack_animation")
+	if anim_sprite.animation != "attack":
+		anim_sprite.play("attack")
+	else:
+		anim_sprite.play()
