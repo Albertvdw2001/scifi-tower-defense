7271ddede1e52d6443cdad3cbf6738ee03247db3
albert@ankerdata.com <albert@ankerdata.com>
2025-12-23 18:12:20 +0200

    Got projectiles workiong with extensions; Reorganized files

---
+++ base_scripts/projectile_base.gd
@@ -0,0 +1,34 @@
+class_name ProjectileBase
+
+extends Area2D
+
+var direction: Vector2 = Vector2.RIGHT
+
+# false if projectile is not a moving projectile (e.g. a bullet) with its own, independant movement
+# true if projectile sticks to weapon (e.g. taser electric shock)
+@export var is_static: bool = false
+
+@export var attack_animation_name: String = "attack_animation"
+
+# speed at which a non-static projectile moves
+@export var movement_speed: float = 500
+
+
+func _physics_process(delta: float) -> void:
+	if is_static:
+		if is_attack_released():
+			queue_free()
+	else:
+		apply_movement(delta)
+
+
+func is_attack_released() -> bool:
+	if not Input.is_action_pressed("attack"):
+		return true
+	else:
+		return false
+
+
+func apply_movement(delta: float):
+	global_position += direction * movement_speed * delta
+	
--- character_extensions/taser_ext.gd
+++ character_extensions/taser_ext.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
-var projectile
+@export var projectile: PackedScene
+@export var is_static_projectile: bool = true
+
+@onready var projectile_slot: Node2D = $ProjectileSlot
 
 func _physics_process(delta: float) -> void:
 	apply_look_point_direction()
+	if (Input.is_action_just_pressed("attack")):
+		spawn_projectile()
 
-
+# points extension front x to where mouse is
 func apply_look_point_direction():
 	var mouse_pos = get_global_mouse_position()
 	look_at(mouse_pos)
+
+# spawns (or "instantiates") a new instance of object assigned to 'projectile'
+# inside 'projectile_slot'
+func spawn_projectile():
+	var new_projectile = projectile.instantiate() as ProjectileBase
+	new_projectile.is_static = is_static_projectile
+	if is_static_projectile:
+		#new_projectile.position = projectile_slot.position
+		projectile_slot.add_child(new_projectile)
+	else:
+		new_projectile.direction = Vector2.RIGHT.rotated(projectile_slot.global_rotation)
+		new_projectile.global_position = projectile_slot.global_position
+		get_tree().current_scene.add_child(new_projectile)
+
+	
